5884be2061d0730eba28d54bb5899fadab4f76e6
Nexus <nexus.riot@gmail.com>
2016-09-05 00:46:20 +0300

    Command handlers

--- bot.py
+++ bot.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
-import config
+
+import datetime
 import telebot
-import time
+from bot_api.config import token
 
 
-def listener(messages):
-    for m in messages:
-        if m.content_type == 'text':
-            bot.send_message(m.chat.id, m.text)
+bot = telebot.TeleBot(token, threaded=False)
 
-if __name__ == '__main__':
-    bot = telebot.TeleBot(config.token)
-    bot.set_update_listener(listener)
-    bot.polling(none_stop=True)
-    while True:
-        time.sleep(200)
+
+@bot.message_handler(commands=['help'])
+def handle_help_command(message):
+    bot.send_message(message.from_user.id, """
+    /start - Start the bot\n""")
+
+
+@bot.message_handler(commands=['start'])
+def handle_help_command(message):
+    bot.send_message(message.from_user.id, "Started")
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == 'date':
+        bot.send_message(message.from_user.id,
+                         datetime.datetime.now())
+
+
+bot.polling(none_stop=True, interval=1)
